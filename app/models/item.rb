c319a60acbfff365d4fff505faf55f29575a7df1
kouki.yamamura <kouki0218@outlook.jp>
2019-03-21 09:08:11 +0000

    want

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,7 @@ class Item < ApplicationRecord
     validates :url, presence: true, length:{maximum:255}
     validates :image_url ,presence: true, length:{maximum:255}
     
+    
+    has_many :wants
+    has_many :want_users, through: :wants, source: :user
 end
