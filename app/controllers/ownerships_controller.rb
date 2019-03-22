79587b52e9d466dff2f1aec99c6088d7db112dcd
kouki.yamamura <kouki0218@outlook.jp>
2019-03-22 03:31:50 +0000

    for heroku

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -16,6 +16,11 @@ class OwnershipsController < ApplicationController
       current_user.want(@item)
       flash[:success] = '商品を Want しました。'
     end
+    
+    if params[:type] =='Have'
+      current_user.have(@item)
+      flash[:success] ='商品を have しました。'
+    end
 
     redirect_back(fallback_location: root_path)
   end
@@ -26,6 +31,11 @@ class OwnershipsController < ApplicationController
     if params[:type] == 'Want'
       current_user.unwant(@item) 
       flash[:success] = '商品の Want を解除しました。'
+    end
+    
+    if params[:type] == 'Have'
+      current_user.unhave(@item) 
+      flash[:success] = '商品の Have を解除しました。'
     end
 
     redirect_back(fallback_location: root_path)
--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,9 @@ class Item < ApplicationRecord
     
     has_many :wants
     has_many :want_users, through: :wants, source: :user
+    
+    has_many :haves ,class_name: 'Have'
+    has_many :have_users, through: :haves ,class_name: 'User' ,source: :user
+    
+    has_many :ownerships, foreign_key: 'item_id', dependent: :destroy
 end
