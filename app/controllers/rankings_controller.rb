aac840ab285693e0eb1bc8e0f67049af8cd7d308
kouki.yamamura <kouki0218@outlook.jp>
2019-03-22 06:33:09 +0000

    ranking

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -2,6 +2,10 @@ class RankingsController < ApplicationController
   def want
     @ranking_counts = Want.ranking
     @items = Item.find(@ranking_counts.keys)
-    
+  end
+  
+  def have
+    @ranking_counts = Have.ranking
+    @items = Item.find(@ranking_counts.keys)
   end
 end
